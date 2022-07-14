ca5a9b0e8ee87d0d0efde10387740e1e58b1243f
Dinesh Sawant <dineshsawant300@gmail.com>
2022-07-14 21:50:16 +0530

    allure example

--- features/support/env.rb
+++ features/support/env.rb
@@ -40,3 +40,25 @@ Capybara.run_server = false
 at_exit do
   @bs_local.stop unless @bs_local.nil? 
 end
+
+require "allure-cucumber"
+
+AllureCucumber.configure do |config|
+  config.results_directory = "report/allure-results"
+  config.clean_results_directory = true
+  # config.logging_level = Logger::INFO
+  # config.logger = Logger.new($stdout, Logger::DEBUG)
+  # config.environment = "staging"
+
+  # these are used for creating links to bugs or test cases where {} is replaced with keys of relevant items
+  config.link_tms_pattern = "http://www.jira.com/browse/{}"
+  config.link_issue_pattern = "http://www.jira.com/browse/{}"
+
+  # additional metadata
+  # environment.properties
+  # config.environment_properties = {
+  #   custom_attribute: "foo"
+  # }
+  # categories.json
+  # config.categories = File.new("my_custom_categories.json")
+end
